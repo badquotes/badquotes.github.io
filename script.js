119b33bddce59ecf3a534954ab9f20014a19af0d
Julio Cesar <julioocamargoo@gmail.com>
2017-05-25 15:33:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var synth = window.speechSynthesis;
 
-var phrases = ["que bad", "uuúhf que bad", "abstrai meu pau no seu cu", "é memo?", "eu só queria ser feliz", "moisés, tá ligado?", "hot", "tô com um caibro tão tão inacreditável enfiado no rabo", "na minha opinião de merda"];
+var phrases = ["que bad", "uuúhf que bad", "abstrai meu pau no seu cu", "é memo?", "eu só queria ser feliz", "tô igual o moisés tá ligado?", "hot", "tô com um caibro tão tão inacreditável enfiado no rabo", "na minha opinião de merda", "fullstack é o caralho"];
 
 function getRandomPhrase(){
 	return phrases[Math.floor(Math.random() * phrases.length)];
@@ -10,6 +10,6 @@ function touchWill(){
   var phrase = new SpeechSynthesisUtterance(getRandomPhrase());
   phrase.voice = synth.getVoices()[16]; //pt-br Google
 	phrase.pitch = 0;
-	phrase.rate = 0.8;
+	phrase.rate = 1;
   synth.speak(phrase);
 }
