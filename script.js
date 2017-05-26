8e1b4a333313e0afb37f94bcf870c4e7968b07f6
Julio Cesar <julioocamargoo@gmail.com>
2017-05-26 11:07:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var synth = window.speechSynthesis;
 
-var phrases = ["que bad", "uuúhf que bad", "abstrai meu pau no seu cu", "é memo?", "eu só queria ser feliz", "tô igual o moisés tá ligado?", "hot", "tô com um caibro tão tão inacreditável enfiado no rabo", "na minha opinião de merda", "fullstack é o caralho"];
+var phrases = ["que bad", "uuúhf que bad", "abstrai meu pau no seu cu", "é memo?", "eu só queria ser feliz", "tô igual o moisés tá ligado?", "tô com um caibro tão tão inacreditável enfiado no rabo", "na minha opinião de merda", "fullstack é o caralho"];
 
 function getRandomPhrase(){
 	return phrases[Math.floor(Math.random() * phrases.length)];
