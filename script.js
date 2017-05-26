2613fc6dce0b11c0254dbdaa874f197458d68a3b
Daniel Felizi <felizi.daniel@gmail.com>
2017-05-26 13:47:13 -0300

    Atualização de frases

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 var synth = window.speechSynthesis;
 
-var phrases = ["que bad", "uuúhf que bad", "abstrai meu pau no seu cu", "é memo?", "eu só queria ser feliz", "tô igual o moisés tá ligado?", "tô com um caibro tão tão inacreditável enfiado no rabo", "na minha opinião de merda", "fullstack é o caralho"];
+var phrases = [
+								"que bad", 
+								"uuúhf que bad", 
+								"abstrai meu pau no seu cu", 
+								"é memo?", 
+								"eu só queria ser feliz", 
+								"tô igual o moisés tá ligado?", 
+								"tô com um caibro tão tão inacreditável enfiado no rabo", 
+								"na minha opinião de merda", 
+								"fullstack é o caralho",
+								"júst that",
+								"menos converseiro e mais trampo",
+								"ai ai",
+								"atchiiiiiiiiiiiiiiiiiiim",
+								"bando de moises",
+								"rio de janeiro",
+								"p j",
+								"eu falo muita merda",
+								"hoje eu tô numa bad",
+								"tóp",
+								"tá ligado?",
+								"um truta meu",
+								"maluuuuco",
+								"bora tomar um café lá",
+								"não sou back end, tá ligado?",
+								"isso que dá back end fazer front end"
+							];
 
 function getRandomPhrase(){
 	return phrases[Math.floor(Math.random() * phrases.length)];
